c161b24ca56f09a43518dda00b229f9c6d9c7771
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2026-07-17 22:24:04 -0300

    feat: G Key

--- src/main/java/com/felpslipe/dungeonutils/DungeonUtils.java
+++ src/main/java/com/felpslipe/dungeonutils/DungeonUtils.java
@@ -1,10 +1,13 @@
 package com.felpslipe.dungeonutils;
 
 import com.felpslipe.dungeonutils.features.DmitriAnnouncer;
+import com.felpslipe.dungeonutils.features.GKey;
 import meteordevelopment.orbit.EventBus;
 import meteordevelopment.orbit.IEventBus;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +25,15 @@ public class DungeonUtils implements ClientModInitializer {
         client = Minecraft.getInstance();
         eventBus.registerLambdaFactory("com.felpslipe.dungeonutils", (lookupInMethod, klass) -> (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
         eventBus.subscribe(DmitriAnnouncer.class);
+        GKey.register();
         LOGGER.info("[DungeonUtils] Ass");
-
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (GKey.GKEY.consumeClick()) {
+                if (client.player != null) {
+                    client.keyboardHandler.setClipboard(GKey.secret);
+                    client.player.displayClientMessage(Component.literal("Gave 1 [Ghost Block] to ").append(client.player.getName()), false);
+                }
+            }
+        });
 	}
 }
